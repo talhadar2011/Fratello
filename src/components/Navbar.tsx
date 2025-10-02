805ad5feb6d4c47d2deaf595cc0fc7cc054bd4ef
Talha <talhadar2011@gmail.com>
2025-10-02 12:43:31 +0200

    navBar options Added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import logo from "../assets/FratelloLogoFullBlack.png"
 export default function Navbar() {
-  return (
-    <>
-        <nav className='bg-gray-200 w-auto  shadow-xl'>
-           <div>
-            <img className='w-40 p-5' src={logo}alt=''></img>
-           </div> 
-        </nav>
-    </>
-  )
+    return (
+        <>
+            <nav className='bg-white w-auto  shadow-xl sticky'>
+                <div className='flex items-center justify-around'>
+                    <img className='w-40 p-5 ' src={logo} alt='fratelloLogo'></img>
+                    <div className='flex gap-6 mx-auto font-medium'>
+                        <h4 className='text-lg hover:underline cursor-pointer'>Home</h4>
+                        <h4 className='text-lg hover:underline cursor-pointer'>Products</h4>
+                        <h4 className='text-lg hover:underline cursor-pointer'>AboutUs</h4>
+                        <h4 className='text-lg hover:underline cursor-pointer'>Contact</h4>
+
+                    </div>
+
+                </div>
+            </nav>
+        </>
+    )
 }
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+// tailwind.config.js
+export default {
+  // ... other configuration
+  theme: {
+    extend: {
+      fontFamily: {
+        // Define a custom font utility. You can name this anything you want.
+        'fratello': ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  // ...
+}
